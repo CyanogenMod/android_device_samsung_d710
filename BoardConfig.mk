825cb479e5bffd70f9dd702d37717b03b5c880b5
Dorian Snyder <dastin1015@gmail.com>
2013-11-29 11:30:49 -0800

    d710: add more selinux policies
    Change-Id: I253cae1690dc38ed67f64c119eeff008fe2dc068

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,7 +33,10 @@ BOARD_SEPOLICY_DIRS += \
 
 BOARD_SEPOLICY_UNION += \
     device.te \
+    file.te \
     file_contexts \
+    mediaserver.te \
+    system.te \
     wimax.te
 
 # Notification LED
